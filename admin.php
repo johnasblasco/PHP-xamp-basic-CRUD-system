a77ab6ca1799dff12b8027ba3513aca280ffa482
Johnas Blasco <147505768+johnasblasco@users.noreply.github.com>
2023-12-03 19:40:11 +0800

    Update admin.php

--- admin.php
+++ admin.php
@@ -23,29 +23,29 @@
             </li>
 
             <li>
-                <a href="">
+                <a href="#" onclick ="displayContainer('news')">
                     News
                 </a>
             </li>
 
             <li>
-                <a href="">
+                <a href="#" onclick ="displayContainer('events')">
                     Events
                 </a>
             </li>
 
             <li>
-                <a href="">
+                <a href="#" onclick ="displayContainer('Announcement')">
                     Announcement
                 </a>
             </li>
             <li>
-                <a href="">
+                 <a href="#" onclick ="displayContainer('Product')">
                     Product
                 </a>
             </li>
             <li>
-                <a href="">
+                 <a href="#" onclick ="displayContainer('Service')">
                     Service
                 </a>
             </li>
@@ -77,10 +77,10 @@
             <th>Action</th>
             </tr>
             <?php
-                $servername = "localhost";
-                $username = "root";
-                $password = "";
-                $database = "kuys";
+                $servername = "sql213.infinityfree.com";
+                $username = "if0_35499800";
+                $password = "QUZXiw4MQuACY";
+                $database = "if0_35499800_kuys";
 
                 $connection = new mysqli($servername,$username,$password,$database);
 
@@ -128,10 +128,10 @@
             <th>Action</th>
             </tr>
             <?php
-                $servername = "localhost";
-                $username = "root";
-                $password = "";
-                $database = "kuys";
+                $servername = "sql213.infinityfree.com";
+                $username = "if0_35499800";
+                $password = "QUZXiw4MQuACY";
+                $database = "if0_35499800_kuys";
 
                 $connection = new mysqli($servername,$username,$password,$database);
 
@@ -152,13 +152,13 @@
                 while($row = $result->fetch_assoc()){
                     echo "
                     <tr>
-                        <td>$row[customerID]</td>
+                        <td>$row[id]</td>
                         <td>$row[name]</td>
                         <td>$row[email]</td>
                         <td>$row[addr]</td>
                         <td>
-                            <a href = 'edit-customer.php?id=$row[customerID]'><button id ='ed'>EDIT</button></a>
-                            <a href = 'delete-customer  .php?id=$row[customerID]'><button id = 'del'>DELETE</button></a>
+                            <a href = 'edit-customer.php?id=$row[id]'><button id ='ed'>EDIT</button></a>
+                            <a href = 'delete-customer.php?id=$row[id]'><button id = 'del'>DELETE</button></a>
                         </td>
                 </tr>
                     ";
@@ -185,10 +185,142 @@
                 </tr>
             </tbody>
             
-        </div>
+        </table>
 
+        <table class = "news" id = "news">
+            <thead>
+                <tr>
+                    <th>News</th>
+                </tr>
+            </thead>        
+            
+            <tbody style ="background-color: #F4F27E;">
+                <tr>
+                   <th> nov 30 2023 <br> BREAKING NEWS! WEBSITE IS OPEN. BUT BUG, FEATURES, WILL STILL FIXING </th>
+                </tr>
+                <tr>
+                <td>
+                    nov 31 2023 <br>new food will release soon
+                    <br>
+                    some people say it will be the best food they ever tasted.
+                    </td>
+                </tr>
+                <tr>
+                    <td>dec 1 2023 <br> supply of meats will be demand by the end of this year.</td>
+                </tr>
 
+                <tr>
+                    <td>dec 2  2023 <br> Hiring employees 25$ per hour..</td>
+                </tr>
+                <tr>
+                    <td>dec 3  2023 <br> People agree to eat salad than eat rice this survey was achieve by asking random 100 people.</td>
+                </tr>
+               <tr>
+                    <td>dec 3  2023 <br> News still updating.</td>
+                </tr>
+
+            </tbody>
+            
+        </table>
+
+        <table class = "events" id = "events">
+            <thead>
+                <tr>
+                    <th>Events</th>
+                </tr>
+            </thead>        
+            
+            <tbody style ="background-color: #F4F27E;">
+                <tr>
+                   <th>  nov 31 2023 <br>No EVENTS TODAY </th>
+                </tr>
+                <tr>
+                    <td>
+                    dec 1 2023
+                    <br>
+                    FOOD IS GOOD SALE EVENTS. THATS WHY MANY PEOPLE EAT.
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                    dec 5 2023
+                    <br>
+                    CHRISTMAS SALE 20% off 
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <table class = "Announcement" id = "Announcement">
+            <thead>
+                <tr>
+                    <th>Announcement</th>
+                </tr>
+            </thead>        
+            
+            <tbody style ="background-color: #F4F27E;">
+                <tr>
+                   <th>  nov 31 2023 <br>DISHES WILL UPDATE COMING SOON </th>
+                </tr>
+                <tr>
+                    <td>
+                        dec 1 2023
+                        <br>
+                        FOOD IS GOOD EVENTS ( 60% off all dish)
+                    </td>
+                </tr>
+                <tr>
+                     <td>
+                        dec 2 2023
+                        <br>
+                        NEW DISH REVEAL
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+
+        <table class = "Product" id = "Product">
+            <thead>
+                <tr>
+                    <td style ="background-color: black; color: blue">PRODUCT</td>
+                </tr>
+                <tr>
+                    <th>Sweet Corn</th>
+                </tr>
+                <tr>
+                    <th>Gulay</th>
+                </tr>
+                <tr>
+                    <th>Omelette</th>
+                </tr>
+                <tr>
+                    <th>Patty</th>
+                </tr>
+                <tr>
+                    <th>Four Season</th>
+                </tr>
+                <tr>
+                    <th>Salad</th>
+                </tr>
+                <tr>
+                    <th>Breakfast</th>
+                </tr>
+                <tr>
+                    <th>Blueberry Dish</th>
+                </tr>
+                <tr>
+                    <th>Ceasar Salad</th>
+                </tr>
+            </thead>   
+        </table>
         
+        <table class = "Service" id = "Service">
+            <thead>
+                <tr>
+                    <td style ="background-color: black; color: white; font-weight: bolder">SERVICE <br> this website running as self service</td>
+                </tr>
+            </thead>
+        </table>
 
         <!-- end of customer table -->
 </body>
